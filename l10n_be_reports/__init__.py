e95ebd575afa69dd68c4bdaea12b74db3ccd61ed
pvy <pvy@odoo.com>
2015-06-17 17:29:29 +0200

    [ADD] l10n_be_reports

---
+++ l10n_be_reports/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+import xml_export
+import partner_vat_listing
---
+++ l10n_be_reports/__openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Belgium - Accounting Reports',
+    'version': '1.1',
+    'category': 'Localization/Account Reports',
+    'author': 'Odoo SA',
+    'description': """
+        Accounting reports for Belgium
+    """,
+    'depends': [
+        'l10n_be',
+    ],
+    'data': [
+        'account_financial_report.xml',
+        'report_vat_statement.xml',
+    ],
+    'installable': True,
+    'auto_install': True,
+    'website': 'https://www.odoo.com/page/accounting',
+}
