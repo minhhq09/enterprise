59bd70494e135f1eff745bed3fb40e31755a2e51
cod-odoo <cod@openerp.com>
2015-06-22 15:03:06 +0200

    [ADD] l10n_ar_reports

---
+++ l10n_ar_reports/__openerp__.py
@@ -0,0 +1,24 @@
+# -*- encoding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+# Copyright (c) 2011 Cubic ERP - Teradata SAC. (http://cubicerp.com)
+
+{
+    'name': 'Argentina - Accounting Reports',
+    'version': '1.1',
+    'description': """
+Accounting reports for Argentina
+================================
+
+    """,
+    'author': ['Cubic ERP'],
+    'website': 'http://cubicERP.com',
+    'category': 'Localization/Account Charts',
+    'depends': ['l10n_ar'],
+    'data':[
+        'report_vat_statement.xml'
+    ],
+    'demo': [],
+    'auto_install': True,
+    'installable': True,
+}
