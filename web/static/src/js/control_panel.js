2017eb4ade07655361bf6a72af1dba147b10ac60
Aaron Bohy <aab@odoo.com>
2015-07-03 13:45:59 +0200

    [REF] web: ControlPanel
    Adds the option clear so that, if set to false, only some elements of the
    control panel can be updated while keeping the other ones unchanged.
    The breadcrumbs can then be updated by simply calling update_control_panel(),
    instead of triggering a specific event to do it.
    
    A bit of documentation and refactoring as well, mainly for the
    breadcrumbs to be handled as other control panel's elements.

--- web/static/src/js/control_panel.js
+++ web/static/src/js/control_panel.js
@@ -17,9 +17,10 @@ var ControlPanelMixin = {
     /**
      * Triggers 'update' on the cp_bus to update the ControlPanel according to cp_status
      * @param {Object} [cp_status] see web.ControlPanel.update() for a description
+     * @param {Object} [options] see web.ControlPanel.update() for a description
      */
-    update_control_panel: function(cp_status) {
-        this.cp_bus.trigger("update", cp_status || {});
+    update_control_panel: function(cp_status, options) {
+        this.cp_bus.trigger("update", cp_status || {}, options || {});
     },
 };
 
@@ -48,23 +49,25 @@ var ControlPanel = Widget.extend({
 
         this.bus = new core.Bus();
         this.bus.on("update", this, this.update);
-        this.bus.on("update_breadcrumbs", this, this.update_breadcrumbs);
     },
+    /**
+     * Renders the control panel and creates a dictionnary of its exposed elements
+     * @return {jQuery.Deferred}
+     */
     start: function() {
-        this.$breadcrumbs = this.$('.breadcrumb');
-
         // Exposed jQuery nodesets
         this.nodes = {
+            $breadcrumbs: this.$('.breadcrumb'),
+            $buttons: this.$('.o_cp_buttons'),
+            $pager: this.$('.o_cp_pager'),
             $searchview: this.$('.o_cp_searchview'),
             $searchview_buttons: this.$('.o_search_options'),
-            $buttons: this.$('.o_cp_buttons'),
             $sidebar: this.$('.o_cp_sidebar'),
-            $pager: this.$('.o_cp_pager'),
             $switch_buttons: this.$('.o_cp_switch_buttons'),
         };
 
         // By default, hide the ControlPanel and remove its contents from the DOM
-        this.toggle_visibility(false);
+        this._toggle_visibility(false);
 
         return this._super();
     },
@@ -75,10 +78,57 @@ var ControlPanel = Widget.extend({
         return this.bus;
     },
     /**
-     * Hides (or shows) the ControlPanel in headless (resp. non-headless) mode
+     * Updates the content and display of the ControlPanel
+     * @param {Object} [status.active_view] the current active view
+     * @param {Array} [status.breadcrumbs] the breadcrumbs to display (see _render_breadcrumbs() for
+     * precise description)
+     * @param {Object} [status.cp_content] dictionnary containing the new ControlPanel jQuery elements
+     * @param {Boolean} [status.hidden] true if the ControlPanel should be hidden
+     * @param {openerp.web.SearchView} [status.searchview] the searchview widget
+     * @param {Boolean} [status.search_view_hidden] true if the searchview is hidden, false otherwise
+     * @param {Boolean} [options.clear] set to true to clear from control panel
+     * elements that are not in status.cp_content
+     */
+    update: function(status, options) {
+        this._toggle_visibility(!status.hidden);
+
+        // Don't update the ControlPanel in headless mode as the views have
+        // inserted themselves the buttons where they want, so inserting them
+        // again in the ControlPanel will removed them from there they should be
+        if (!status.hidden) {
+            options = _.defaults({}, options, {
+                clear: true, // clear control panel by default
+            });
+            var new_cp_content = status.cp_content || {};
+
+            // Render the breadcrumbs
+            if (status.breadcrumbs) {
+                new_cp_content.$breadcrumbs = this._render_breadcrumbs(status.breadcrumbs);
+            }
+
+            // Detach control_panel old content and attach new elements
+            if (options.clear) {
+                this._detach_content(this.nodes);
+                // Show the searchview buttons area, which might have been hidden by
+                // the searchview, as client actions may insert elements into it
+                this.nodes.$searchview_buttons.show();
+            } else {
+                this._detach_content(_.pick(this.nodes, _.keys(new_cp_content)));
+            }
+            this._attach_content(new_cp_content);
+
+            // Update the searchview and switch buttons
+            this._update_search_view(status.searchview, status.search_view_hidden);
+            if (status.active_view_selector) {
+                this._update_switch_buttons(status.active_view_selector);
+            }
+        }
+    },
+    /**
+     * Private function that hides (or shows) the ControlPanel in headless (resp. non-headless) mode
      * Also detaches or attaches its contents to clean the DOM
      */
-    toggle_visibility: function(visible) {
+    _toggle_visibility: function(visible) {
         this.$el.toggle(visible);
         if (!visible && !this.$content) {
             this.$content = this.$el.contents().detach();
@@ -88,15 +138,16 @@ var ControlPanel = Widget.extend({
         }
     },
     /**
-     * Detaches the content of the ControlPanel
+     * Private function that detaches the content of the ControlPanel
+     * @param {Object} [elements_to_detach] subset of this.nodes to detach
      */
-    _detach_content: function() {
-        _.each(this.nodes, function($nodeset) {
+    _detach_content: function(elements_to_detach) {
+        _.each(elements_to_detach, function($nodeset) {
             $nodeset.contents().detach();
         });
     },
     /**
-     * Attaches content to the ControlPanel
+     * Private function that attaches content to the ControlPanel
      * @param {Object} [content] dictionnary of jQuery elements to attach, whose keys
      * are jQuery nodes identifiers in this.nodes
      */
@@ -104,61 +155,39 @@ var ControlPanel = Widget.extend({
         var self = this;
         _.each(content, function($nodeset, $element) {
             if ($nodeset && self.nodes[$element]) {
-                $nodeset.appendTo(self.nodes[$element]);
+                self.nodes[$element].append($nodeset);
             }
         });
     },
     /**
-     * Updates the content and display of the ControlPanel
-     * @param {Object} [status.active_view] the current active view
-     * @param {Array} [status.breadcrumbs] the breadcrumbs to display
-     * @param {Object} [status.cp_content] dictionnary containing the new ControlPanel jQuery elements
-     * @param {Boolean} [status.hidden] true if the ControlPanel should be hidden
-     * @param {openerp.web.SearchView} [status.searchview] the searchview widget
-     * @param {Boolean} [status.search_view_hidden] true if the searchview is hidden, false otherwise
-     */
-    update: function(status) {
-        this.toggle_visibility(!status.hidden);
-        if (!status.hidden) {
-            // Don't update the ControlPanel in headless mode as the views have
-            // inserted themselves the buttons where they want, so inserting them
-            // again in the ControlPanel will removed them from there they should be
-            this._detach_content();
-            this._attach_content(status.cp_content);
-            this.update_search_view(status.searchview, status.search_view_hidden);
-            if (status.active_view_selector) this.update_switch_buttons(status.active_view_selector);
-            if (status.breadcrumbs) this.update_breadcrumbs(status.breadcrumbs);
-        }
-    },
-    /**
-     * Removes active class on all switch-buttons and adds it to the one of the active view
+     * Private function that removes active class on all switch-buttons and adds
+     * it to the one of the active view
      * @param {Object} [active_view_selector] the selector of the div to activate
      */
-    update_switch_buttons: function(active_view_selector) {
+    _update_switch_buttons: function(active_view_selector) {
         _.each(this.nodes.$switch_buttons.find('button'), function(button) {
             $(button).removeClass('active');
         });
         this.$(active_view_selector).addClass('active');
     },
     /**
-     * Updates the breadcrumbs
-     **/
-    update_breadcrumbs: function (breadcrumbs) {
+     * Private function that renders the breadcrumbs
+     * @param {Array} [breadcrumbs] list of objects containing the following keys:
+     *      - action: the action to execute when clicking on this part of the breadcrumbs
+     *      - index: the index in the breadcrumbs (starting at 0)
+     *      - title: what to display in the breadcrumbs
+     * @return {Array} list of breadcrumbs' li jQuery elements
+     */
+    _render_breadcrumbs: function (breadcrumbs) {
         var self = this;
 
-        if (!breadcrumbs.length) return;
-
-        var $breadcrumbs = breadcrumbs.map(function (bc, index) {
+        return breadcrumbs.map(function (bc, index) {
             return make_breadcrumb(bc, index, breadcrumbs.length);
         });
 
-        this.$breadcrumbs
-            .empty()
-            .append($breadcrumbs);
-
         function make_breadcrumb (bc, index, length) {
             var is_last = (index === length-1);
-            var is_before_last = index === length-2
+            var is_before_last = (index === length-2);
 
             var $bc = $('<li>')
                     .append(is_last ? bc.title : $('<a>').html(bc.title))
@@ -174,11 +203,12 @@ var ControlPanel = Widget.extend({
         }
     },
     /**
-     * Updates the SearchView's visibility and extend the breadcrumbs area if the SearchView is not visible
+     * Private function that pdates the SearchView's visibility and extend the
+     * breadcrumbs area if the SearchView is not visible
      * @param {openerp.web.SearchView} [searchview] the searchview Widget
      * @param {Boolean} [is_hidden] visibility of the searchview
-     **/
-    update_search_view: function(searchview, is_hidden) {
+     */
+    _update_search_view: function(searchview, is_hidden) {
         if (searchview) {
             this.nodes.$searchview.toggle(!is_hidden);
             // Set the $buttons div (in the DOM) of the searchview as the $buttons
@@ -193,17 +223,13 @@ var ControlPanel = Widget.extend({
                     .addClass('o_enable_searchview btn btn-sm btn-default fa fa-search')
                     .on('click', function() {
                         self.$el.toggleClass('o_breadcrumb_full');
-                });
+                    });
             }
             if(!is_hidden && config.device.xs) {
                 this.$enable_searchview.insertAfter(this.nodes.$searchview);
             } else {
                 this.$enable_searchview.detach();
             }
-        } else {
-            // Show the searchview buttons area, which might have been hidden by
-            // the searchview, as client actions may insert elements into it
-            this.nodes.$searchview_buttons.show();
         }
     },
 });
