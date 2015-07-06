f2f798a30b23c3164ca5319b74f8dd77b912b99c
rsi-odoo <rsi@openerp.com>
2015-07-06 12:23:35 +0530

    [ADD] l10n_hr_reports

---
+++ l10n_hr_reports/__openerp__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+# Copyright (C) 2011- Slobodni programi d.o.o., Zagreb
+
+{
+    'name': 'Croatia - RRIF 2012 COA - Accounting Reports',
+    'version': '1.1',
+    'description': """
+Accounting reports for Croatia
+================================
+    """,
+    'author': ['OpenERP Croatian Community'],
+    'website': 'https://code.launchpad.net/openobject-croatia',
+    'category': 'Localization/Account Charts',
+    'depends': ['l10n_hr'],
+    'data': [
+        'account_financial_html_report.xml'
+    ],
+    'demo': [],
+    'auto_install': True,
+    'installable': True,
+}
