f243c103e474493409124086c14f2364150500b8
qdp-odoo <qdp@odoo.com>
2015-08-20 17:29:09 +0200

    [FIX] account_bank_statement_import_ofx: fixed warning 'unkown field acc_number on account.journal'

--- account_bank_statement_import_ofx/tests/test_import_bank_statement.py
+++ account_bank_statement_import_ofx/tests/test_import_bank_statement.py
@@ -13,7 +13,7 @@ class TestOfxFile(TransactionCase):
         # Create a bank account and journal corresponding to the OFX file (same currency and account number)
         bank_journal_id = self.env['account.journal'].create({'name': 'Bank 123456', 'code': 'BNK67', 'type': 'bank',
             'currency_id': self.env.ref("base.USD").id,
-            'acc_number': '123456'}).id
+            'bank_acc_number': '123456'}).id
 
         # Use an import wizard to process the file
         import_wizard = self.env['account.bank.statement.import'].with_context(journal_id=bank_journal_id).create({'data_file': ofx_file})
