5bd2f377b17625a425152625cf8152f613531249
Damien Bouvy <dbo@odoo.com>
2015-08-27 15:33:42 +0200

    [IMP] sale_contract_asset: sale refactoring compatibility Model name change and view changes

--- sale_contract_asset/models/account_analytic_account.py
+++ sale_contract_asset/models/account_analytic_account.py
@@ -1,17 +1,17 @@
 from openerp import models, fields, api
 
 
-class account_analytic_account(models.Model):
-    _name = "account.analytic.account"
-    _inherit = "account.analytic.account"
+class SaleSubscription(models.Model):
+    _name = "sale.subscription"
+    _inherit = "sale.subscription"
 
     asset_category_id = fields.Many2one('account.asset.category', 'Deferred Revenue Category',
                                         help="This asset category will be applied to the lines of the contract's invoices.",
                                         domain="[('type','=','sale')]")
 
     @api.multi
-    def on_change_template(self, template_id, date_start=False):
-        res = super(account_analytic_account, self).on_change_template(template_id, date_start)
+    def on_change_template(self, template_id):
+        res = super(SaleSubscription, self).on_change_template(template_id)
 
         template = self.browse(template_id)
         if template.asset_category_id:
@@ -21,7 +21,7 @@ class account_analytic_account(models.Model):
 
     @api.model
     def _prepare_invoice_lines(self, contract, fiscal_position_id):
-        inv_lines = super(account_analytic_account, self)._prepare_invoice_lines(contract, fiscal_position_id)
+        inv_lines = super(SaleSubscription, self)._prepare_invoice_lines(contract, fiscal_position_id)
 
         for line in inv_lines:
             line[2]['asset_category_id'] = contract.asset_category_id and contract.asset_category_id.id
