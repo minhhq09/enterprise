6ebbd699beeedfa5f0e0cf54759b852073a2bdca
pvy <pvy@odoo.com>
2015-09-01 16:51:44 +0200

    [ADD] l10n_jp_reports

---
+++ l10n_jp_reports/__openerp__.py
@@ -0,0 +1,23 @@
+# -*- encoding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+# Copyright (C) Rooms For (Hong Kong) Limited T/A OSCG
+
+{
+    'name': 'Japan - Accounting Reports',
+    'version': '1.1',
+    'description': """
+Accounting reports for Japan
+================================
+    """,
+    'author': ['Rooms For (Hong Kong) Limited T/A OSCG'],
+    'website': 'http://www.openerp-asia.net/',
+    'category': 'Localization/Account Charts',
+    'depends': ['l10n_jp'],
+    'data': [
+        'account_financial_html_report.xml'
+    ],
+    'demo': [],
+    'auto_install': True,
+    'installable': True,
+}
