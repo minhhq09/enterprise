cfcb37bbd560bfbd7bada7b2b4729827956129ce
cod-odoo <cod@openerp.com>
2015-09-02 16:24:08 +0200

    [ADD] l10n_in_reports

---
+++ l10n_in_reports/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- encoding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Indian - Accounting Reports',
+    'version': '1.1',
+    'description': """
+Accounting reports for India
+================================
+    """,
+    'author': ['OpenERP SA'],
+    'category': 'Localization/Account Charts',
+    'depends': ['l10n_in'],
+    'data': [
+        'account_financial_html_report.xml'
+    ],
+    'demo': [],
+    'auto_install': True,
+    'installable': True,
+}
