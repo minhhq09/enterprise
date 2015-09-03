ce7c4447cdd82068953341828fd01808b65d1fb7
cod-odoo <cod@opernerp.com>
2015-09-03 11:16:16 +0200

    [ADD] l10n_sg_reports

---
+++ l10n_sg_reports/__openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Singapore - Accounting Reports',
+    'version': '1.1',
+    'author': 'Tech Receptives',
+    'website': 'http://www.techreceptives.com',
+    'category': 'Localization/Account Report',
+    'description': """
+Accounting reports for Singapore
+================================
+    """,
+    'depends': [
+        'l10n_sg'
+    ],
+    'data': [
+        'account_financial_html_report.xml',
+    ],
+    'installable': True,
+    'auto_install': True,
+}
