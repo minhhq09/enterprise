125f5192948ebc036305ed976c16b3b8a53e470e
cod-odoo <cod@opernerp.com>
2015-09-03 15:15:10 +0200

    [ADD] l10n_fr_reports

---
+++ l10n_fr_reports/__openerp__.py
@@ -0,0 +1,24 @@
+# -*- encoding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+# Copyright (c) 2008 JAILLET Simon - CrysaLEAD - www.crysalead.fr
+
+{
+    'name': 'France - Accounting Reports',
+    'version': '1.1',
+    'description': """
+Accounting reports for France
+================================
+
+    """,
+    'author': ['OpenERP SA'],
+    'website': 'http://www.openerp.com',
+    'category': 'Localization/Account Charts',
+    'depends': ['l10n_fr'],
+    'data':[
+        'account_financial_html_report.xml'
+    ],
+    'demo': [],
+    'auto_install': True,
+    'installable': True,
+}
