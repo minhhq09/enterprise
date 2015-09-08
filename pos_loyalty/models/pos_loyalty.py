2755852d0d85f341cb8314b5f1d1ea85359b44e0
Deep Bundela <dbu@odoo.com>
2015-09-08 14:54:21 +0530

    [MOV] pos_loyalty: Splitted class files as per new API

--- pos_loyalty/models/pos_loyalty.py
+++ pos_loyalty/models/pos_loyalty.py
@@ -44,7 +44,6 @@ class loyalty_rule(osv.osv):
         'type':'product',
     }
 
-
 class loyalty_reward(osv.osv):
     _name = 'loyalty.reward'
     _columns = {
@@ -85,40 +84,3 @@ class loyalty_reward(osv.osv):
         (_check_discount_product, "The discount product field is mandatory for discount rewards", ["type","discount_product_id"]),
         (_check_point_product,    "The point product field is mandatory for point resale rewards", ["type","discount_product_id"]),
     ]
-
-class pos_config(osv.osv):
-    _inherit = 'pos.config' 
-    _columns = {
-        'loyalty_id': fields.many2one('loyalty.program','Loyalty Program', help='The loyalty program used by this point_of_sale'),
-        
-    }
-
-class res_partner(osv.osv):
-    _inherit = 'res.partner'
-    _columns = {
-        'loyalty_points': fields.float('Loyalty Points', help='The loyalty points the user won as part of a Loyalty Program')
-    }
-
-class pos_order(osv.osv):
-    _inherit = 'pos.order'
-
-    _columns = {
-        'loyalty_points': fields.float('Loyalty Points', help='The amount of Loyalty points the customer won or lost with this order'),
-    }
-
-    def _order_fields(self, cr, uid, ui_order, context=None):
-        fields = super(pos_order,self)._order_fields(cr,uid,ui_order,context)
-        fields['loyalty_points'] = ui_order.get('loyalty_points',0)
-        return fields
-
-    def create_from_ui(self, cr, uid, orders, context=None):
-        ids = super(pos_order,self).create_from_ui(cr,uid,orders,context=context)
-        for order in orders:
-            if order['data']['loyalty_points'] != 0 and order['data']['partner_id']:
-                partner = self.pool.get('res.partner').browse(cr,uid,order['data']['partner_id'], context=context)
-                partner.write({'loyalty_points': partner['loyalty_points'] + order['data']['loyalty_points']})
-
-        return ids
-            
-             
-    
