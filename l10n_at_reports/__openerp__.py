563ffd09d08b34c4f9287098866f99315592d095
cod-odoo <cod@openerp.com>
2015-09-09 12:42:03 +0530

    [ADD] l10n_at_reports

---
+++ l10n_at_reports/__openerp__.py
@@ -0,0 +1,22 @@
+# -*- encoding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+# Copyright (C) conexus.at
+
+{
+    'name': 'Austria - Accounting',
+    'version': '1.0',
+    'author': 'conexus.at',
+    'website': 'http://www.conexus.at',
+    'category': 'Localization/Account Charts',
+    'depends': ['l10n_at'],
+    'description': """
+This module provides the standard Accounting Chart for Austria which is based on the Template from BMF.gv.at.
+============================================================================================================= 
+Please keep in mind that you should review and adapt it with your Accountant, before using it in a live Environment.
+""",
+    'demo': [],
+    'data': ['report_vat_statement.xml'],
+    'auto_install': True,
+    'installable': True
+}
