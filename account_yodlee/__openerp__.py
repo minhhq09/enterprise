9b2d7b742e921ad02c171bb6cb9d5b3938a95565
Richard Mathot <rim@odoo.com>
2015-09-11 16:09:24 +0200

    [FIX] it is better with a module description...

--- account_yodlee/__openerp__.py
+++ account_yodlee/__openerp__.py
@@ -1,19 +1,11 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "account_yodlee",
-
-    'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
-
-    'description': """
-        Long description of module's purpose
-    """,
-
-    'author': "Your Company",
-    'website': "http://www.yourcompany.com",
-    'category': 'Uncategorized',
-    'version': '0.1',
+    'name': "Yodlee",
+    'summary': "Yodlee Finance",
+    'author': "Odoo S.A.",
+    'website': "https://www.odoo.com",
+    'category': 'Accounting &amp; Finance',
+    'version': '1.0',
     'depends': ['account_online_sync'],
     'data': [
         'views/yodlee_views.xml',
