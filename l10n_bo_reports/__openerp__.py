289a2d3f18d1844f630c6dcf2ae9734c3e9bfd64
cod-odoo <cod@openerp.com>
2015-09-12 21:29:02 +0530

    [ADD] l10n_bo_reports

---
+++ l10n_bo_reports/__openerp__.py
@@ -0,0 +1,24 @@
+# -*- encoding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+# Copyright (c) 2011 Cubic ERP - Teradata SAC. (https://cubicerp.com).
+
+{
+    "name": "Bolivia - Accounting Reports",
+    "version": "1.1",
+    "description": """
+Accounting reports for Bolivia
+================================
+
+    """,
+    "author": "Cubic ERP",
+    "website": "https://cubicERP.com",
+    "category": "Localization/Account Charts",
+    "depends": ["l10n_bo"],
+    "data": [
+        "account_financial_html_report.xml",
+    ],
+    "auto_install": True,
+    "installable": True,
+    "license": "OEEL-1",
+}
