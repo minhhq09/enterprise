6bcde701174483884e92102151dada832b398368
Richard Mathot <rim@odoo.com>
2015-09-15 17:16:35 +0200

    [IMP] delivery_fedex: clean tests

--- delivery_fedex/tests/test_delivery_fedex.py
+++ delivery_fedex/tests/test_delivery_fedex.py
@@ -1,16 +1,12 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
+import unittest
 from openerp.tests.common import TransactionCase
 
 
 class TestDeliveryFedex(TransactionCase):
 
     def setUp(self):
-        # We do not want to overload FedEx with runbot's requests, change this
-        # to True in order to test this module
-        self.run_tests = False
-        # self.run_tests = True
-
         super(TestDeliveryFedex, self).setUp()
 
         self.iPod16 = self.env.ref('product.product_product_8')
@@ -28,49 +24,8 @@ class TestDeliveryFedex(TransactionCase):
         self.agrolait.write({'country_id': self.env.ref('base.be').id})
         self.delta_pc = self.env.ref('base.res_partner_4')
 
-    def test_01_fedex_basic_international_flow(self):
-        if not self.run_tests:
-            return True
-
-        SaleOrder = self.env['sale.order']
-
-        sol_vals = {'product_id': self.iPod16.id,
-                    'name': "[A6678] iPod (16 GB)",
-                    'product_uom_qty': 1.0}
-
-        so_vals = {'partner_id': self.agrolait.id,
-                   'carrier_id': self.env.ref('delivery_fedex.delivery_carrier_fedex_inter').id,
-                   'order_policy': 'picking',
-                   'order_line': [(0, None, sol_vals)]}
-
-        sale_order = SaleOrder.create(so_vals)
-        self.assertGreater(sale_order.delivery_price, 0.0, "FedEx delivery cost for this SO has not been correctly estimated.")
-
-        sale_order.action_confirm()
-        self.assertEquals(len(sale_order.picking_ids), 1, "The Sale Order did not generate a picking.")
-
-        picking = sale_order.picking_ids[0]
-        self.assertEquals(picking.carrier_id.id, sale_order.carrier_id.id, "Carrier is not the same on Picking and on SO.")
-
-        picking.force_assign()
-
-        picking.do_prepare_partial()
-        self.assertGreater(picking.weight, 0.0, "Picking weight should be positive.")
-
-        picking.do_transfer()
-
-        self.assertIsNot(picking.carrier_tracking_ref, False, "FedEx did not return any tracking number")
-        self.assertGreater(picking.carrier_price, 0.0, "FedEx carrying price is probably incorrect")
-
-        picking.cancel_shipment()
-
-        self.assertFalse(picking.carrier_tracking_ref, "Carrier Tracking code has not been properly deleted")
-        self.assertEquals(picking.carrier_price, 0.0, "Carrier price has not been properly deleted")
-
-    def test_02_fedex_basic_us_domestic_flow(self):
-        if not self.run_tests:
-            return True
-
+    @unittest.skip("Fedex test disabled: We do not want to overload Fedex with runbot's requests")
+    def test_01_fedex_basic_us_domestic_flow(self):
         SaleOrder = self.env['sale.order']
 
         sol_vals = {'product_id': self.iPod16.id,
@@ -107,10 +62,45 @@ class TestDeliveryFedex(TransactionCase):
         self.assertFalse(picking.carrier_tracking_ref, "Carrier Tracking code has not been properly deleted")
         self.assertEquals(picking.carrier_price, 0.0, "Carrier price has not been properly deleted")
 
-    def test_03_fedex_multipackage_international_flow(self):
-        if not self.run_tests:
-            return True
+    @unittest.skip("Fedex test disabled: We do not want to overload Fedex with runbot's requests")
+    def test_02_fedex_basic_international_flow(self):
+        SaleOrder = self.env['sale.order']
 
+        sol_vals = {'product_id': self.iPod16.id,
+                    'name': "[A6678] iPod (16 GB)",
+                    'product_uom_qty': 1.0}
+
+        so_vals = {'partner_id': self.agrolait.id,
+                   'carrier_id': self.env.ref('delivery_fedex.delivery_carrier_fedex_inter').id,
+                   'order_policy': 'picking',
+                   'order_line': [(0, None, sol_vals)]}
+
+        sale_order = SaleOrder.create(so_vals)
+        self.assertGreater(sale_order.delivery_price, 0.0, "FedEx delivery cost for this SO has not been correctly estimated.")
+
+        sale_order.action_confirm()
+        self.assertEquals(len(sale_order.picking_ids), 1, "The Sale Order did not generate a picking.")
+
+        picking = sale_order.picking_ids[0]
+        self.assertEquals(picking.carrier_id.id, sale_order.carrier_id.id, "Carrier is not the same on Picking and on SO.")
+
+        picking.force_assign()
+
+        picking.do_prepare_partial()
+        self.assertGreater(picking.weight, 0.0, "Picking weight should be positive.")
+
+        picking.do_transfer()
+
+        self.assertIsNot(picking.carrier_tracking_ref, False, "FedEx did not return any tracking number")
+        self.assertGreater(picking.carrier_price, 0.0, "FedEx carrying price is probably incorrect")
+
+        picking.cancel_shipment()
+
+        self.assertFalse(picking.carrier_tracking_ref, "Carrier Tracking code has not been properly deleted")
+        self.assertEquals(picking.carrier_price, 0.0, "Carrier price has not been properly deleted")
+
+    @unittest.skip("Fedex test disabled: We do not want to overload Fedex with runbot's requests")
+    def test_03_fedex_multipackage_international_flow(self):
         SaleOrder = self.env['sale.order']
 
         sol_1_vals = {'product_id': self.iPod16.id,
