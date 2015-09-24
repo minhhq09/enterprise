38a89d73a499d58a2ea85a8a9f4345c1bf62a1f8
Arthur Maniet <arthurmaniet@me.com>
2015-09-24 17:10:43 +0200

    [REF] stock_barcode: improve main controller readability

--- stock_barcode/controllers/main.py
+++ stock_barcode/controllers/main.py
@@ -8,48 +8,64 @@ class StockBarcodeController(http.Controller):
         """ Receive a barcode scanned from the main menu and return the appropriate
             action (open an existing / new picking) or warning.
         """
-        # Get the action to open a picking form as a dict
-        action_picking_form = request.env.ref('stock_barcode.stock_picking_action_form')
-        action_picking_form = action_picking_form.read()[0]
+        ret_open_picking = try_open_picking(barcode)
+        if ret_open_picking:
+            return ret_open_picking
 
-        # If the barcode represents a picking, open it
-        corresponding_picking = request.env['stock.picking'].search([
-            ('name', '=', barcode),
-            ('state', 'in', ('partially_available', 'assigned'))
-        ], limit=1)
-        if corresponding_picking:
-            action_picking_form['res_id'] = corresponding_picking.id
-            return {'action': action_picking_form}
-
-        # If the barcode represents a location, open a new picking from this location
-        corresponding_location = request.env['stock.location'].search([
-            ('barcode', '=', barcode),
-            ('usage', '=', 'internal')
-        ], limit=1)
-        if corresponding_location:
-            internal_picking_type = request.env['stock.picking.type'].search([('code', '=', 'internal')])
-            warehouse = request.env['stock.location'].get_warehouse(corresponding_location)
-            if warehouse:
-                internal_picking_type = internal_picking_type.filtered(lambda r: r.warehouse_id.id == warehouse)
-            dest_loc = corresponding_location
-            while dest_loc.location_id and dest_loc.location_id.usage == 'internal':
-                dest_loc = dest_loc.location_id
-            if internal_picking_type:
-                # Create and confirm an internal picking
-                picking = request.env['stock.picking'].create({
-                    'picking_type_id': internal_picking_type[0].id,
-                    'location_id': corresponding_location.id,
-                    'location_dest_id': dest_loc.id,
-                })
-                picking.action_confirm()
-
-                # Open its form view
-                action_picking_form.update(res_id=picking.id)
-                return {'action': action_picking_form}
-            else:
-                return {'warning': _('No internal picking type. Please configure one in warehouse settings.')}
+        ret_new_internal_picking = try_new_internal_picking(barcode)
+        if ret_new_internal_picking:
+            return ret_new_internal_picking
 
         return {'warning': _('No picking or location corresponding to barcode %(barcode)s') % {'barcode': barcode}}
+
+
+def try_open_picking(barcode):
+    """ If barcode represents a picking, open it
+    """
+    corresponding_picking = request.env['stock.picking'].search([
+        ('name', '=', barcode),
+        ('state', 'in', ('partially_available', 'assigned'))
+    ], limit=1)
+    if corresponding_picking:
+        action_picking_form = request.env.ref('stock_barcode.stock_picking_action_form')
+        action_picking_form = action_picking_form.read()[0]
+        action_picking_form['res_id'] = corresponding_picking.id
+        return {'action': action_picking_form}
+    return False
+
+
+def try_new_internal_picking(barcode):
+    """ If barcode represents a location, open a new picking from this location
+    """
+    corresponding_location = request.env['stock.location'].search([
+        ('barcode', '=', barcode),
+        ('usage', '=', 'internal')
+    ], limit=1)
+    if corresponding_location:
+        internal_picking_type = request.env['stock.picking.type'].search([('code', '=', 'internal')])
+        warehouse = request.env['stock.location'].get_warehouse(corresponding_location)
+        if warehouse:
+            internal_picking_type = internal_picking_type.filtered(lambda r: r.warehouse_id.id == warehouse)
+        dest_loc = corresponding_location
+        while dest_loc.location_id and dest_loc.location_id.usage == 'internal':
+            dest_loc = dest_loc.location_id
+        if internal_picking_type:
+            # Create and confirm an internal picking
+            picking = request.env['stock.picking'].create({
+                'picking_type_id': internal_picking_type[0].id,
+                'location_id': corresponding_location.id,
+                'location_dest_id': dest_loc.id,
+            })
+            picking.action_confirm()
+
+            # Open its form view
+            action_picking_form = request.env.ref('stock_barcode.stock_picking_action_form')
+            action_picking_form = action_picking_form.read()[0]
+            action_picking_form.update(res_id=picking.id)
+            return {'action': action_picking_form}
+        else:
+            return {'warning': _('No internal picking type. Please configure one in warehouse settings.')}
+    return False
 
 
     @http.route('/stock_barcode/rid_of_message_demo_barcodes', type='json', auth='user')
