6173232a1dfbfb5c7ac00e6856e01c8653a7c511
Csaba Fazekas <csabafazekasster@gmail.com>
2015-09-24 13:26:50 +0200

    [IMP] website_sign: frontend dialog design

--- website_sign/static/src/js/website_sign_frontend.js
+++ website_sign/static/src/js/website_sign_frontend.js
@@ -365,7 +365,7 @@ odoo.define('website_sign.frontend', function(require) {
 
         init: function(parent, options) {
             options = (options || {});
-            options.title = options.title || _t("Thank You !");
+            options.title = options.title || _t("Thank You !") + "<br/>";
             options.subtitle = options.subtitle || _t("Your signature has been saved.");
             options.size = options.size || "medium";
 
