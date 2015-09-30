701d24892bbd65a3f2f1d8fdc54fa46deaec2134
Richard Mathot <rim@odoo.com>
2015-09-30 08:35:55 +0200

    [FIX] delivery_fedex: tests have not been properly ported to 9.0

--- delivery_fedex/tests/test_delivery_fedex.py
+++ delivery_fedex/tests/test_delivery_fedex.py
@@ -9,17 +9,11 @@ class TestDeliveryFedex(TransactionCase):
     def setUp(self):
         super(TestDeliveryFedex, self).setUp()
 
-        self.iPod16 = self.env.ref('product.product_product_8')
-        self.iMac = self.env.ref('product.product_product_11')
+        self.iPadMini = self.env.ref('product.product_product_6')
+        self.iMac = self.env.ref('product.product_product_8')
+        self.uom_unit = self.env.ref('product.product_uom_unit')
 
-        # Set weight
-        self.iPod16.weight = 0.5
-        self.iMac.weight = 11
-
-        # Add a full address to "Your Company"
         self.your_company = self.env.ref('base.main_partner')
-        self.your_company.write({'country_id': self.env.ref('base.us').id,
-                                 'state_id': self.env.ref('base.state_us_39').id})
         self.agrolait = self.env.ref('base.res_partner_2')
         self.agrolait.write({'country_id': self.env.ref('base.be').id})
         self.delta_pc = self.env.ref('base.res_partner_4')
@@ -28,13 +22,14 @@ class TestDeliveryFedex(TransactionCase):
     def test_01_fedex_basic_us_domestic_flow(self):
         SaleOrder = self.env['sale.order']
 
-        sol_vals = {'product_id': self.iPod16.id,
-                    'name': "[A6678] iPod (16 GB)",
-                    'product_uom_qty': 1.0}
+        sol_vals = {'product_id': self.iPadMini.id,
+                    'name': "[A1232] iPad Mini",
+                    'product_uom': self.uom_unit.id,
+                    'product_uom_qty': 1.0,
+                    'price_unit': self.iPadMini.lst_price}
 
         so_vals = {'partner_id': self.delta_pc.id,
                    'carrier_id': self.env.ref('delivery_fedex.delivery_carrier_fedex_us').id,
-                   'order_policy': 'picking',
                    'order_line': [(0, None, sol_vals)]}
 
         sale_order = SaleOrder.create(so_vals)
@@ -49,9 +44,9 @@ class TestDeliveryFedex(TransactionCase):
 
         picking.force_assign()
 
-        picking.do_prepare_partial()
         self.assertGreater(picking.weight, 0.0, "Picking weight should be positive.")
 
+        picking.pack_operation_product_ids.qty_done = 1.0
         picking.do_transfer()
 
         self.assertIsNot(picking.carrier_tracking_ref, False, "FedEx did not return any tracking number")
@@ -66,13 +61,14 @@ class TestDeliveryFedex(TransactionCase):
     def test_02_fedex_basic_international_flow(self):
         SaleOrder = self.env['sale.order']
 
-        sol_vals = {'product_id': self.iPod16.id,
-                    'name': "[A6678] iPod (16 GB)",
-                    'product_uom_qty': 1.0}
+        sol_vals = {'product_id': self.iPadMini.id,
+                    'name': "[A1232] iPad Mini",
+                    'product_uom': self.uom_unit.id,
+                    'product_uom_qty': 1.0,
+                    'price_unit': self.iPadMini.lst_price}
 
         so_vals = {'partner_id': self.agrolait.id,
                    'carrier_id': self.env.ref('delivery_fedex.delivery_carrier_fedex_inter').id,
-                   'order_policy': 'picking',
                    'order_line': [(0, None, sol_vals)]}
 
         sale_order = SaleOrder.create(so_vals)
@@ -85,10 +81,9 @@ class TestDeliveryFedex(TransactionCase):
         self.assertEquals(picking.carrier_id.id, sale_order.carrier_id.id, "Carrier is not the same on Picking and on SO.")
 
         picking.force_assign()
-
-        picking.do_prepare_partial()
         self.assertGreater(picking.weight, 0.0, "Picking weight should be positive.")
 
+        picking.pack_operation_product_ids.qty_done = 1.0
         picking.do_transfer()
 
         self.assertIsNot(picking.carrier_tracking_ref, False, "FedEx did not return any tracking number")
@@ -103,16 +98,19 @@ class TestDeliveryFedex(TransactionCase):
     def test_03_fedex_multipackage_international_flow(self):
         SaleOrder = self.env['sale.order']
 
-        sol_1_vals = {'product_id': self.iPod16.id,
-                      'name': "[A6678] iPod (16 GB)",
-                      'product_uom_qty': 1.0}
+        sol_1_vals = {'product_id': self.iPadMini.id,
+                      'name': "[A1232] iPad Mini",
+                      'product_uom': self.uom_unit.id,
+                      'product_uom_qty': 1.0,
+                      'price_unit': self.iPadMini.lst_price}
         sol_2_vals = {'product_id': self.iMac.id,
                       'name': "[A1090] iMac",
-                      'product_uom_qty': 1.0}
+                      'product_uom': self.uom_unit.id,
+                      'product_uom_qty': 1.0,
+                      'price_unit': self.iMac.lst_price}
 
         so_vals = {'partner_id': self.agrolait.id,
                    'carrier_id': self.env.ref('delivery_fedex.delivery_carrier_fedex_inter').id,
-                   'order_policy': 'picking',
                    'order_line': [(0, None, sol_1_vals), (0, None, sol_2_vals)]}
 
         sale_order = SaleOrder.create(so_vals)
@@ -125,16 +123,14 @@ class TestDeliveryFedex(TransactionCase):
         self.assertEquals(picking.carrier_id.id, sale_order.carrier_id.id, "Carrier is not the same on Picking and on SO.")
 
         picking.force_assign()
-        picking.do_prepare_partial()
-        # This is done to mimic the barcode interface
-        # We put pack each product separately
-        po0 = picking.pack_operation_ids[0].id
-        po1 = picking.pack_operation_ids[1].id
-        picking.process_product_id_from_ui(picking.id, self.iPod16.id, po0)
-        picking.action_pack(operation_filter_ids=[po0])
-        picking.process_product_id_from_ui(picking.id, self.iMac.id, po1)
-        picking.action_pack(operation_filter_ids=[po1])
-        # End
+
+        po0 = picking.pack_operation_product_ids[0]
+        po1 = picking.pack_operation_product_ids[1]
+        po0.qty_done = 1
+        picking.put_in_pack()
+        po1.qty_done = 1
+        picking.put_in_pack()
+
         self.assertGreater(picking.weight, 0.0, "Picking weight should be positive.")
         self.assertTrue(all([po.result_package_id is not False for po in picking.pack_operation_ids]), "Some products have not been put in packages")
 
