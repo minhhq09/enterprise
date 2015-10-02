e3d2a80e37f65c4f01b32166097f3685212ee4e0
Olivier Dony <odo@openerp.com>
2015-10-02 20:02:19 +0200

    [REM] account_extensions: remove duplication config settings
    Those settings have been moved to the `account` module
    in the Community Edition, so they were being displayed
    twice.

--- account_extension/__openerp__.py
+++ account_extension/__openerp__.py
@@ -1,19 +1,17 @@
 # -*- coding: utf-8 -*-
 {
     'name': "Account Extension",
-    'summary': """Add some extension to account modules for enterprise version""",
     'description': """
-        Add to res_config of account modules the possibility to install extra accounting modules
+        Adapt base accounting module for Enterprise Edition.
 
-        This module will be auto installed if account module and enterprise version are present
+        This module will be auto installed if account module and enterprise edition are present
     """,
     'author': "Odoo SA",
-    'category': 'Accounting &amp; Finance',
+    'category': 'Hidden',
     'version': '1.0',
     'depends': ['account'],
     'data': [
         'views/account_report_menu_invisible.xml',
-        'views/res_config_view.xml',
     ],
     'auto_install': True,
     'license': 'OEEL-1',
