ed78fce1797aa930c61f49fc90180c72d05d3921
Arthur Maniet <arthurmaniet@me.com>
2015-10-15 16:51:30 +0200

    [ADD] account_SEPA: constraints to ensure proper bank accounts config
    Check that both 'from' and 'to' bank accounts, if of type IBAN, have a BIC
    
    This goes along with odoo/odoo@15a4d587987fcc7c01087c5074360b1f6ed39c24

--- account_sepa/account_payment.py
+++ account_sepa/account_payment.py
@@ -42,6 +42,24 @@ class AccountPayment(models.Model):
                 raise ValidationError(_("A SEPA communication cannot exceed 140 characters"))
             check_valid_SEPA_str(self.communication)
 
+    @api.one
+    @api.constrains('payment_method_id', 'journal_id')
+    def _check_bank_account(self):
+        if self.payment_method_id == self.env.ref('account_sepa.account_payment_method_sepa_ct'):
+            if not self.journal_id.bank_account_id or not self.journal_id.bank_account_id.acc_type == 'iban':
+                raise ValidationError(_("The journal '%s' requires a proper IBAN account to pay via SEPA. Please configure it first.") % self.journal_id.name)
+            if not self.journal_id.bank_account_id.bank_bic:
+                raise ValidationError(_("The account '%s' (journal %s) requires a Bank Identification Code (BIC) to pay via SEPA. Please configure it first.")
+                    % (self.journal_id.bank_account_id.acc_number, self.journal_id.name))
+
+    @api.one
+    @api.constrains('payment_method_id', 'partner_bank_account_id')
+    def _check_partner_bank_account(self):
+        if self.payment_method_id == self.env.ref('account_sepa.account_payment_method_sepa_ct'):
+            # Note, the condition allows to use non-IBAN account. SEPA actually supports this under certain conditions
+            if self.partner_bank_account_id.acc_type == 'iban' and not self.partner_bank_account_id.bank_bic:
+                raise ValidationError(_("The partner account '%s' requires a Bank Identification Code (BIC) to pay via SEPA. Please configure it first.") % self.journal_id.bank_account_id.acc_number)
+
     @api.onchange('partner_id')
     def _onchange_partner_id(self):
         if hasattr(super(AccountPayment, self), '_onchange_partner_id'):
