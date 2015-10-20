minhhq09/enterprise

commit 0cde33d97704584ff17f9d6d4f2c38f21e2f985a

[FIX] account_SEPA: enable SEPA only on journals labelled in euro

account_sepa/account_journal.py
@@ -14,5 +14,10 @@ class AccountJournal(models.Model):
         """ Enables sepa credit transfer payment method on bank journals. Called upon module installation via data file.
         """
         sepa_ct = self.env.ref('account_sepa.account_payment_method_sepa_ct')
-        for bank_journal in self.search([('type', '=', 'bank')]):
+        euro = self.env.ref('base.EUR')
+        if self.env.user.company_id.currency_id == euro:
+            domain = ['&', ('type', '=', 'bank'), '|', ('currency_id', '=', euro.id), ('currency_id' '=', False)]
+        else:
+            domain = ['&', ('type', '=', 'bank'), ('currency_id', '=', euro.id)]
+        for bank_journal in self.search(domain):
             bank_journal.write({'outbound_payment_method_ids': [(4, sepa_ct.id, None)]})

commit 8bc452f4188a07bd9bc189a4a8963cd870ff6015

[FIX] account_sepa: typo in domain causing error
Introduced by 0cde33d.

account_sepa/account_journal.py
@@ -16,7 +16,7 @@ class AccountJournal(models.Model):
         sepa_ct = self.env.ref('account_sepa.account_payment_method_sepa_ct')
         euro = self.env.ref('base.EUR')
         if self.env.user.company_id.currency_id == euro:
-            domain = ['&', ('type', '=', 'bank'), '|', ('currency_id', '=', euro.id), ('currency_id' '=', False)]
+            domain = ['&', ('type', '=', 'bank'), '|', ('currency_id', '=', euro.id), ('currency_id', '=', False)]
         else:
             domain = ['&', ('type', '=', 'bank'), ('currency_id', '=', euro.id)]
         for bank_journal in self.search(domain):