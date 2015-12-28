b11add6eddb58b928b766d1b9ac87f0d28b71bd1
Martin Trigaux <mat@odoo.com>
2015-12-28 10:35:46 +0100

    [ADD] l10n_multilang_report: make report name translatable
    Only in multilang to be sure it will only be available for multilang CoA.
    Doesn't make much sense to translate Belgian reports in Macedonian...
    opw 662226

---
+++ l10n_multilang_report/__openerp__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Multi Language Accounting Reports',
+    'version': '1.1',
+    'category': 'Hidden/Dependency',
+    'description': """
+    * Multi language support for Financial Reports
+    """,
+    'depends' : ['l10n_multilang', 'account_reports'],
+    'data': [],
+    'demo': [],
+    'installable': True,
+    'auto_install': True,
+}
