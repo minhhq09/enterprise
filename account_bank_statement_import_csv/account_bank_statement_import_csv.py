5b2a9766816ed55f9088adb42c83008424e894c9
Cedric Snauwaert <csn@openerp.com>
2016-04-11 11:07:15 +0200

    [FIX] account_bank_stmt_import_csv: fix error when balance was not defined
    + converting value to float should ignore empty
    + removing element from list should be done at the end to prevent some errors

--- account_bank_statement_import_csv/account_bank_statement_import_csv.py
+++ account_bank_statement_import_csv/account_bank_statement_import_csv.py
@@ -46,7 +46,7 @@ class AccountBankStmtImportCSV(models.TransientModel):
             add_fields = [{
                 'id': 'balance',
                 'name': 'balance',
-                'string': 'Balance',
+                'string': 'Cumulative Balance',
                 'required': False,
                 'fields': [],
                 'type': 'monetary',
@@ -68,37 +68,49 @@ class AccountBankStmtImportCSV(models.TransientModel):
             fields.extend(add_fields)
         return fields
 
+    def _convert_to_float(self, value):
+        return float(value) if value else 0.0
+
     def _parse_import_data(self, cr, uid, data, import_fields, record, options, context=None):
         data = super(AccountBankStmtImportCSV, self)._parse_import_data(cr, uid, data, import_fields, record, options, context)
         statement_id = context.get('bank_statement_id', False)
         if statement_id:
             import_fields.append('statement_id/.id')
+            index_balance = False
             convert_to_amount = False
             if 'debit' in import_fields and 'credit' in import_fields:
                 index_debit = import_fields.index('debit')
                 index_credit = import_fields.index('credit')
                 self._parse_float_from_data(cr, uid, data, index_debit, 'debit', options, context=context)
                 self._parse_float_from_data(cr, uid, data, index_credit, 'credit', options, context=context)
-                import_fields.remove('debit')
-                import_fields.remove('credit')
                 import_fields.append('amount')
                 convert_to_amount = True
             # add starting balance and ending balance to context
             if 'balance' in import_fields:
                 index_balance = import_fields.index('balance')
                 self._parse_float_from_data(cr, uid, data, index_balance, 'balance', options, context=context)
-                context['starting_balance'] = float(data[0][index_balance])
-                context['starting_balance'] -= float(data[0][import_fields.index('amount')]) if not convert_to_amount else float(data[0][index_debit])-float(data[0][index_credit])
+                context['starting_balance'] = self._convert_to_float(data[0][index_balance])
+                context['starting_balance'] -= self._convert_to_float(data[0][import_fields.index('amount')]) \
+                                                if not convert_to_amount \
+                                                else self._convert_to_float(data[0][index_debit])-self._convert_to_float(data[0][index_credit])
                 context['ending_balance'] = data[len(data)-1][index_balance]
                 import_fields.remove('balance')
+            # Remove debit/credit field from import_fields
+            if convert_to_amount:
+                import_fields.remove('debit')
+                import_fields.remove('credit')
+                
             for line in data:
                 line.append(statement_id)
+                remove_index = []
                 if convert_to_amount:
-                    line.append(float(line[index_debit])-float(line[index_credit]))
-                    line.remove(line[index_debit])
-                    line.remove(line[index_credit])
+                    line.append(self._convert_to_float(line[index_debit])-self._convert_to_float(line[index_credit]))
+                    remove_index.extend([index_debit, index_credit])
                 if index_balance:
-                    line.remove(line[index_balance])
+                    remove_index.append(index_balance)
+                # Remove added field debit/credit/balance
+                for index in sorted(remove_index, reverse=True):
+                    line.remove(line[index])
             if 'date' in import_fields:
                 context['date'] = data[len(data)-1][import_fields.index('date')]
 
