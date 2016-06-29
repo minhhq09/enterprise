f4b90b895d3c682395e9cdc210353eb5a806ec9e
Josse Colpaert <jco@odoo.com>
2016-06-29 09:52:47 +0200

    [ADD] quality: quality management
    Containing
    
     - Define quality points that will generate quality checks on pickings,
       manufacturing orders or work orders (quality_mrp)
     - Quality alerts can be created independently or related to quality checks
     - Possibility to add a measure to the quality check with a min/max tolerance
     - Define your stages for the quality alerts
    
    Thanks to all people that helped during this development, notably but not
    limited to Chirag A Dodiya (cod@odoo.com), Gaurav Panchal (gan@odoo.com),
    Jignesh Rathod (jir@odoo.com), Mansi Trivedi (mtr@odoo.com), Pariket Trivedi
    (ptr@odoo.com).

---
+++ quality/models/stock_picking.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import api, fields, models, _
+from odoo.exceptions import UserError
+
+
+class StockPicking(models.Model):
+    _inherit = "stock.picking"
+
+    check_ids = fields.One2many('quality.check', 'picking_id', 'Checks')
+    check_todo = fields.Boolean('Pending checks', compute='_compute_check_todo')
+
+    @api.one
+    def _compute_check_todo(self):
+        # TDE: measure_success use ?
+        if any(check.quality_state == 'none' for check in self.check_ids):
+            self.check_todo = True
+
+    @api.multi
+    def do_new_transfer(self):
+        for picking in self:
+            if any([(x.quality_state == 'none') for x in picking.check_ids]):
+                raise UserError(_('You still need to do the quality checks!'))
+        return super(StockPicking, self).do_new_transfer()
+
+    @api.multi
+    def check_quality(self):
+        self.ensure_one()
+        checks = self.check_ids.filtered(lambda check: check.quality_state == 'none')
+        if checks:
+            action = self.env.ref('quality.quality_check_action_small').read()[0]
+            action['context'] = self.env.context
+            action['res_id'] = checks.ids[0]
+            return action
+        return False
