2e24ea6b8ce2e36ac9e146e61aebe693dc53b0bc
fwi-odoo <fwi@odoo.com>
2016-07-14 17:50:09 +0200

    [MIG] account_bank_statement_import_csv: migrate to new api

--- account_bank_statement_import_csv/wizard/account_bank_statement_import_csv.py
+++ account_bank_statement_import_csv/wizard/account_bank_statement_import_csv.py
@@ -1,9 +1,7 @@
 # -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 import base64
-
-import dateutil.parser
-import StringIO
 
 from odoo import api, models
 
