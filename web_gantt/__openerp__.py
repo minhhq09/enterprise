8508dfa74c1c0ae81856e41e3e9b6e3aeaf97984
fwi-odoo <fwi@odoo.com>
2016-07-19 09:58:45 +0200

    [MIG] web_gantt: finish migration: remove openerp tag and rename templates xml file

--- web_gantt/__openerp__.py
+++ web_gantt/__openerp__.py
@@ -1,3 +1,6 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
 {
     'name': 'Web Gantt',
     'category': 'Hidden',
@@ -9,7 +12,7 @@ OpenERP Web Gantt chart view.
     'version': '2.0',
     'depends': ['web'],
     'data' : [
-        'views/web_gantt.xml',
+        'views/web_gantt_templates.xml',
     ],
     'qweb': [
         'static/src/xml/*.xml',
