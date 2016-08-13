c69b5823340694a704cfab4b87aa046504ef2d61
Jeremy Kersten <jke@odoo.com>
2016-08-13 14:32:16 +0200

    [FIX] website_crm_score: fix test to be full new api
    Some test was still using old api behavior.
    Eg: read(cr, uid, id, [fields]) was returning only one dict.
    In new api, always one list of dict.
    
    These tests was broken since commit odoo/odoo@#9c2c54f8

--- website_crm_score/tests/test_assign.py
+++ website_crm_score/tests/test_assign.py
@@ -15,10 +15,10 @@ class test_assign(TestScoring):
         # scoring
         self.website_crm_score.assign_scores_to_leads(cr, uid)
 
-        l0 = self.crm_lead.read(cr, uid, self.lead0, ['score'], None)
-        l1 = self.crm_lead.read(cr, uid, self.lead1, ['score'], None)
-        l2 = self.crm_lead.read(cr, uid, self.lead2, ['score', 'active'], None)
-        l3 = self.crm_lead.read(cr, uid, self.lead3, ['score', 'active'], dict(test_active=False))
+        l0 = self.crm_lead.read(cr, uid, [self.lead0], ['score'], None)[0]
+        l1 = self.crm_lead.read(cr, uid, [self.lead1], ['score'], None)[0]
+        l2 = self.crm_lead.read(cr, uid, [self.lead2], ['score', 'active'], None)[0]
+        l3 = self.crm_lead.read(cr, uid, [self.lead3], ['score', 'active'], dict(test_active=False))[0]
 
         self.assertEqual(l0['score'], 1000, 'scoring failed')
         self.assertEqual(l1['score'], 900, 'scoring failed')
@@ -28,19 +28,19 @@ class test_assign(TestScoring):
         self.assertEqual(l3['active'], False, ' should be archived')
 
         count = self.crm_lead.search_count(cr, uid, [('id', 'in', all_lead_ids)], None)
-        self.assertEqual(count, len(all_lead_ids)-2, 'One lead should be deleted and one archived')
+        self.assertEqual(count, len(all_lead_ids) - 2, 'One lead should be deleted and one archived')
 
         # assignation
         self.team.direct_assign_leads(cr, uid, None)
 
-        l0 = self.crm_lead.read(cr, uid, self.lead0, ['team_id', 'user_id'], None)
-        l1 = self.crm_lead.read(cr, uid, self.lead1, ['team_id', 'user_id'], None)
-        l2 = self.crm_lead.read(cr, uid, self.lead2, ['team_id', 'user_id'], None)
+        l0 = self.crm_lead.read(cr, uid, [self.lead0], ['team_id', 'user_id'], None)[0]
+        l1 = self.crm_lead.read(cr, uid, [self.lead1], ['team_id', 'user_id'], None)[0]
+        l2 = self.crm_lead.read(cr, uid, [self.lead2], ['team_id', 'user_id'], None)[0]
 
-        self.assertEqual(l0['team_id'][0], self.team0, 'assignation failed')
-        self.assertEqual(l1['team_id'][0], self.team1, 'assignation failed')
+        self.assertEqual(l0['team_id'], self.team0, 'assignation failed')
+        self.assertEqual(l1['team_id'], self.team1, 'assignation failed')
         self.assertEqual(l2['team_id'], False, 'assignation failed')
 
-        self.assertEqual(l0['user_id'][0], self.salesmen0, 'assignation failed')
-        self.assertEqual(l1['user_id'][0], self.salesmen1, 'assignation failed')
+        self.assertEqual(l0['user_id'], self.salesmen0, 'assignation failed')
+        self.assertEqual(l1['user_id'], self.salesmen1, 'assignation failed')
         self.assertEqual(l2['user_id'], False, 'assignation failed')
