efa93a9cc73f7996d60a4ffa9832a1a211a1a206
Christophe Matthieu <chm@odoo.com>
2016-08-31 16:05:11 +0200

    [IMP] website_version: convert tours to web_tour.tour

--- website_version/tests/test_ui.py
+++ website_version/tests/test_ui.py
@@ -5,4 +5,4 @@ import odoo.tests
 @odoo.tests.common.post_install(True)
 class TestUi(odoo.tests.HttpCase):
     def test_01_versioning(self):
-        self.phantom_js("/", "odoo.__DEBUG__.services['web.Tour'].run('versioning', 'test')", "odoo.__DEBUG__.services['web.Tour'].tours.versioning", login='admin')
+        self.phantom_js("/", "odoo.__DEBUG__.services['web_tour.tour'].run('versioning')", "odoo.__DEBUG__.services['web_tour.tour'].tours.versioning.ready", login='admin')
