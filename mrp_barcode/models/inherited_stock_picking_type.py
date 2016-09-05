7a63a302a78aa2dfbd241322bae812e5a8867a21
Ravi Gadhia <rga@odoo.com>
2016-09-05 11:20:36 +0530

    [IMP] mrp_barcode: There is no need to override action picking tree here we can create new metho which is resposible to get action of mrp production tree kanban

--- mrp_barcode/models/inherited_stock_picking_type.py
+++ mrp_barcode/models/inherited_stock_picking_type.py
@@ -9,7 +9,6 @@ class StockPickingType(models.Model):
     _inherit = 'stock.picking.type'
 
     @api.multi
-    def get_action_picking_tree_ready_kanban(self):
-        if self.code == 'mrp_operation':
-            return self._get_action('mrp_barcode.mrp_production_kanban_mrp_barcode')
-        return super(StockPickingType, self).get_action_picking_tree_ready_kanban()
+    def get_action_mrp_production_ready_kanban(self):
+        action = self._get_action('mrp_barcode.mrp_production_kanban_mrp_barcode')
+        return action
