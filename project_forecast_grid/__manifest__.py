567d3b3a874726f7630ba137a1f5974ddb0401ed
Thibault Delavallée <tde@openerp.com>
2016-09-14 13:50:55 +0200

    [FIX][IMP] project_forecast_grid: set module as auto install as it is simply adding the grid view on project forecast

--- project_forecast_grid/__manifest__.py
+++ project_forecast_grid/__manifest__.py
@@ -1,21 +1,19 @@
 # -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
 {
     'name': "Forecast Grid View",
-
-    'summary': "",
-
-    'description': """
-    """,
-
+    'summary': "Add Grid view on Project Forecasts",
+    'description': """Bridge module between Project Forecast and Grid View
+""",
     'category': 'Project Management',
-    'version': '0.1',
-
+    'version': '1.0',
     'depends': ['project_forecast', 'grid'],
-
     'data': [
         'views/views.xml',
     ],
     'demo': [
         'demo/demo.xml',
     ],
+    'auto_install': True,
 }
