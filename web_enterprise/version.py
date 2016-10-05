66a84dab848eac5cc16f9df8ab1b28c1ce75b1ca
Christophe Simonis <chs@odoo.com>
2016-10-05 18:32:59 +0200

    [REL] 10.0+e \o/

--- web_enterprise/version.py
+++ web_enterprise/version.py
@@ -7,9 +7,9 @@ import odoo
 # Monkey patch release to set the edition as 'enterprise'
 # ----------------------------------------------------------
 odoo.release.version_info = version_info = odoo.release.version_info[:5] + ('e',)
-version_split = odoo.release.version.split('-', 1)
-odoo.release.version = (version_split[0][:-1] + version_info[5] +
-                           ('-%s' % version_split[1] if len(version_split) > 1 else ''))
+if '-' not in odoo.release.version:     # not already patched by packaging
+    odoo.release.version += '+' + version_info[5]
+
 odoo.service.common.RPC_VERSION_1.update(
     server_version=odoo.release.version,
     server_version_info=odoo.release.version_info)
