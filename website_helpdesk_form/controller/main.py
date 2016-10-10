4360a2dcf7bcc2414ccc3a5c395ab00307b2b1f9
Denis Vermylen (dve) <dve@odoo.com>
2016-10-10 11:55:26 +0200

    [FIX] helpdesk: search public user by ref, not by name
    Might prevent future translation problems.

--- website_helpdesk_form/controller/main.py
+++ website_helpdesk_form/controller/main.py
@@ -12,7 +12,7 @@ class WebsiteForm(WebsiteForm):
     @http.route('''/helpdesk/<model("helpdesk.team", "[('use_website_helpdesk_form','=',True)]"):team>/submit''', type='http', auth="public", website=True)
     def website_helpdesk_form(self, team, **kwargs):
         default_values = {}
-        if request.env.user.partner_id.name != 'Public user':
+        if request.env.user.partner_id != request.env.ref('base.public_partner'):
             default_values['name'] = request.env.user.partner_id.name
             default_values['email'] = request.env.user.partner_id.email
         return request.render("website_helpdesk_form.ticket_submit", {'team': team, 'default_values': default_values})
