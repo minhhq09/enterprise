minhhq09/enterprise

commit 0ba37243984e4c31416e096abe6a17b1d23117e7

[ADD] mrp_barcode : Add new module for barcode scan functionality in mrp + scrap for stock

mrp_barcode/doc/make_barcodes.sh
@@ -1,0 +1,52 @@
+#!/bin/sh
+
+barcode -t 2x7+40+40 -m 50x30 -p A4 -e code128b -o barcodes_demo_data.ps <<BARCODES
+9873031254663
+9789878466514
+1312465423164
+6548978946196
+5478592110363
+7845854641328
+8798465112398
+LOT-000003
+LOT-000004
+LOT-000005
+LOT-000006
+MO/00001
+MO/00002
+MO/00003
+BARCODES
+
+barcode -t 2x6+20+40 -m 30x30 -m 30x30 -p A4 -e code128b -o production_barcodes_actions.ps <<BARCODES
+O-BTN.action_assign
+O-BTN.button_unreserve
+O-BTN.button_plan
+O-BTN.button_unplan
+O-BTN.open_produce_product
+O-BTN.do_produce
+O-BTN.post_inventory
+O-BTN.button_mark_done
+O-BTN.button_scrap
+O-BTN.action_cancel
+O-BTN.save
+BARCODES
+
+barcode -t 2x6+20+40 -m 30x30 -p A4 -e code128b -o workorder_barcodes_actions.ps <<BARCODES
+O-BTN.button_start
+O-BTN.button_pending
+O-BTN.record_production
+O-BTN.button_scrap
+O-BTN.button_finish
+O-BTN.button_unblock
+BARCODES
+
+
+# add title in postscript :
+#
+# /showTitle % stack: str x y
+# {
+#   /Helvetica findfont 12 scalefont setfont
+#   moveto show
+# } def
+#
+# e.g (My Product) 81.65 810 showTitle

stock_barcode/doc/make_barcodes.sh
@@ -17,13 +17,15 @@ LOT-000001
 LOT-000002
 BARCODES
 
-barcode -t 2x5+20+40 -m 30x30 -p A4 -e code128b -o barcodes_actions.ps <<BARCODES
+barcode -t 2x7+40+40 -m 50x30 -p A4 -e code128b -o barcodes_actions.ps <<BARCODES
 O-CMD.MAIN-MENU
 O-CMD.CANCEL
 O-CMD.EDIT
 O-BTN.validate
 O-BTN.print
 O-BTN.put-in-pack
+O-BTN.button_scrap
+O-BTN.save
 O-CMD.PAGER-PREV
 O-CMD.PAGER-NEXT
 O-CMD.PAGER-FIRST

mrp_barcode/__init__.py
@@ -1,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+import controllers
+import models

mrp_barcode/__openerp__.py
@@ -1,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+
+{
+    'name': 'Manufacturing Barcode Scaning',
+    'version': '1.0',
+    'website': 'https://www.odoo.com/page/manufacturing',
+    'category': 'Manufacturing',
+    'summary': 'Add barcode scanning facilities to manufacturing.',
+    'depends': ['mrp_workorder', 'stock_barcode'],
+    'description': """This module adds support for barcodes scanning to the manufacturing system.""",
+    'data': [
+        'views/inherited_mrp_production_views.xml',
+        'views/inherited_mrp_workorder_views.xml',
+        'views/inherited_stock_move_views.xml',
+        'views/inherited_mrp_workcenter_block_view.xml',
+        'views/inherited_picking_type_views.xml',
+        'views/mrp_barcode_templates.xml',
+    ],
+    'qweb': [
+        "static/src/xml/mrp_barcode.xml",
+    ],
+    'demo': [
+        'data/mrp_barcode_demo.xml',
+    ]
+}

commit a5031e4bcfb5bd963789799134dc7295f36f9720

[FIX] stock_barcode, mrp_barcode: barcode save
Since the add of mrp_barcode, a barcode to save the form has been added
but it doesn't work. This barcode doesn't work, because it represent
the string 'O-BTN.save', and the code handles the string 'O-CMD.SAVE'.

So to fix this, we have regenerated the barcodes, with the good string
'O-CMD-SAVE'.

This bug has been introduced in rev: https://github.com/odoo/enterprise/commit/0ba37243984e4c31416e096abe6a17b1d23117e7

mrp_barcode/doc/make_barcodes.sh
@@ -28,7 +28,7 @@ O-BTN.post_inventory
 O-BTN.button_mark_done
 O-BTN.button_scrap
 O-BTN.action_cancel
-O-BTN.save
+O-CMD.SAVE
 BARCODES
 
 barcode -t 2x6+20+40 -m 30x30 -p A4 -e code128b -o workorder_barcodes_actions.ps <<BARCODES

mrp_barcode/doc/production_barcodes_actions.ps
@@ -15,6 +15,7 @@
 % 02112141331211221321311232133111133211222311211241411221241121421121341112411121112241211241142121142121421121221142411121131412331112
 
 (CHECK AVAILABILITY) 50.89 777.45 showTitle
+
 [
 %  height  xpos   ypos  width       height  xpos   ypos  width
    [62.55  50.89 709.45  1.63]      [62.55  53.11 709.45  0.74]
@@ -732,9 +733,9 @@
     moveto show} bind forall
 % End barcode for "O-BTN.action_cancel"
 
-% Printing barcode for "O-BTN.save", scaled  1.50, encoded using "code 128-B"
+% Printing barcode for "O-CMD.SAVE", scaled  1.50, encoded using "code 128-B"
 % The space/bar succession is represented by the following widths (space first):
-% 02112141331211221321311232133111133211222311142121211244112121122141124122331112
+% 02112141331211221321313211131231123131222312131131113233111231321132311132331112
 
 (SAVE) 51.50 143.38  showTitle
 
@@ -745,19 +746,19 @@
    [52.03  74.69  84.97  4.34]      [52.03  79.93  84.97  2.84]
    [52.03  83.67  84.97  1.35]      [52.03  88.91  84.97  2.84]
    [52.03  94.15  84.97  4.34]      [52.03 100.13  84.97  1.35]
-   [52.03 106.12  84.97  1.35]      [52.03 109.86  84.97  2.84]
-   [52.03 117.34  84.97  2.84]      [52.03 122.58  84.97  4.34]
-   [52.03 130.07  84.97  1.35]      [52.03 133.06  84.97  1.35]
-   [52.03 137.55  84.97  4.34]      [52.03 145.78  84.97  2.84]
+   [52.03 106.12  84.97  1.35]      [52.03 112.86  84.97  2.84]
+   [52.03 116.60  84.97  1.35]      [52.03 121.09  84.97  4.34]
+   [52.03 126.32  84.97  2.84]      [52.03 133.06  84.97  1.35]
+   [52.03 136.80  84.97  2.84]      [52.03 143.53  84.97  1.35]
    [52.03 149.52  84.97  1.35]      [52.03 154.76  84.97  2.84]
-   [52.03 161.49  84.97  4.34]      [52.03 165.98  84.97  1.35]
-   [52.03 171.22  84.97  5.84]      [52.03 177.96  84.97  1.35]
-   [52.03 182.44  84.97  1.35]      [52.03 186.93  84.97  1.35]
-   [52.03 190.68  84.97  2.84]      [52.03 201.15  84.97  5.84]
-   [52.03 206.39  84.97  1.35]      [52.03 210.88  84.97  1.35]
-   [52.03 215.37  84.97  1.35]      [52.03 219.11  84.97  2.84]
-   [52.03 224.35  84.97  1.35]      [52.03 231.83  84.97  1.35]
-   [52.03 235.57  84.97  2.84]      [52.03 243.80  84.97  1.35]
+   [52.03 161.49  84.97  4.34]      [52.03 166.73  84.97  2.84]
+   [52.03 171.97  84.97  4.34]      [52.03 176.46  84.97  1.35]
+   [52.03 182.44  84.97  1.35]      [52.03 185.44  84.97  1.35]
+   [52.03 192.17  84.97  2.84]      [52.03 200.40  84.97  4.34]
+   [52.03 204.89  84.97  1.35]      [52.03 208.63  84.97  2.84]
+   [52.03 215.37  84.97  1.35]      [52.03 222.10  84.97  2.84]
+   [52.03 225.84  84.97  1.35]      [52.03 232.58  84.97  2.84]
+   [52.03 239.31  84.97  1.35]      [52.03 242.31  84.97  1.35]
    [59.52 249.04  77.48  2.84]      [59.52 257.27  77.48  4.34]
    [59.52 261.76  77.48  1.35]      [59.52 265.50  77.48  2.84]
 
@@ -766,19 +767,19 @@
 %   char    xpos   ypos fontsize
     [(O)   66.46  70.00 17.96]
     [(-)   82.92  70.00  0.00]
-    [(B)   99.39  70.00  0.00]
-    [(T)  115.85  70.00  0.00]
-    [(N)  132.31  70.00  0.00]
+    [(C)   99.39  70.00  0.00]
+    [(M)  115.85  70.00  0.00]
+    [(D)  132.31  70.00  0.00]
     [(.)  148.77  70.00  0.00]
-    [(s)  165.23  70.00  0.00]
-    [(a)  181.70  70.00  0.00]
-    [(v)  198.16  70.00  0.00]
-    [(e)  214.62  70.00  0.00]
+    [(S)  165.23  70.00  0.00]
+    [(A)  181.70  70.00  0.00]
+    [(V)  198.16  70.00  0.00]
+    [(E)  214.62  70.00  0.00]
 ]   { {} forall dup 0.00 ne {
 	/Helvetica findfont exch scalefont setfont
     } {pop} ifelse
     moveto show} bind forall
-% End barcode for "O-BTN.save"
+% End barcode for "O-CMD.SAVE"
 
 showpage
 

stock_barcode/doc/make_barcodes.sh
@@ -21,11 +21,11 @@ barcode -t 2x7+40+40 -m 50x30 -p A4 -e code128b -o barcodes_actions.ps <<BARCODE
 O-CMD.MAIN-MENU
 O-CMD.CANCEL
 O-CMD.EDIT
+O-CMD.SAVE
 O-BTN.validate
 O-BTN.print
 O-BTN.put-in-pack
 O-BTN.button_scrap
-O-BTN.save
 O-CMD.PAGER-PREV
 O-CMD.PAGER-NEXT
 O-CMD.PAGER-FIRST