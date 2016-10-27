efc4f99a7de3c9a701eec78aa7b422c3ed5eb1e7
Christophe Simonis <chs@odoo.com>
2016-10-27 14:22:10 +0200

    [FIX] web_enterprise: correct version patching
    When using packaged community version with git-based enterprise
    modules, the patching of the version number wasn't effective (as the
    version was already containing a dash). Now explictly search for `+e`
    string in version number.

--- web_enterprise/version.py
+++ web_enterprise/version.py
@@ -6,9 +6,9 @@ import odoo
 # ----------------------------------------------------------
 # Monkey patch release to set the edition as 'enterprise'
 # ----------------------------------------------------------
-odoo.release.version_info = version_info = odoo.release.version_info[:5] + ('e',)
-if '-' not in odoo.release.version:     # not already patched by packaging
-    odoo.release.version += '+' + version_info[5]
+odoo.release.version_info = odoo.release.version_info[:5] + ('e',)
+if '+e' not in odoo.release.verion:     # not already patched by packaging
+    odoo.release.version += '{0}+e{1}{2}'.format(*odoo.release.version.partition('-'))
 
 odoo.service.common.RPC_VERSION_1.update(
     server_version=odoo.release.version,
