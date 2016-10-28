30cea2bebb9cbc6cae8a702be41fbb6ac3c317d4
Damien Bouvy <dbo@odoo.com>
2016-10-28 08:11:50 +0200

    [FIX] web_enterprise: correct version patching (ter)µ
    Avoid Odoo 10.0-2016102710.0+e-20161027 as version number,
    it seems redundant.

--- web_enterprise/version.py
+++ web_enterprise/version.py
@@ -8,7 +8,7 @@ import odoo
 # ----------------------------------------------------------
 odoo.release.version_info = odoo.release.version_info[:5] + ('e',)
 if '+e' not in odoo.release.version:     # not already patched by packaging
-    odoo.release.version += '{0}+e{1}{2}'.format(*odoo.release.version.partition('-'))
+    odoo.release.version = '{0}+e{1}{2}'.format(*odoo.release.version.partition('-'))
 
 odoo.service.common.RPC_VERSION_1.update(
     server_version=odoo.release.version,
