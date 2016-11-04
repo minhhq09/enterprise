0212b741d8f39d13584ed403b9df8737a3bbc8d1
Aaron Bohy <aab@odoo.com>
2016-11-04 11:47:20 +0100

    [FIX] web_studio: don't create ir.model.data twice at import
    When records of models customizable in Studio (like ir.model,
    ir.model.fields...) are created or edited, we check if the key 'studio'
    is in the context, and if so, we create an ir.model.data entry for that
    record (if it doesn't exist yet) and flag it with studio.
    
    However, this shouldn't be done when importing a module, as in that
    case, an ir.model.data is automatically created afterwards, leading to
    duplicate entries in ir.model.data.

--- web_studio/models/studio_mixin.py
+++ web_studio/models/studio_mixin.py
@@ -7,6 +7,9 @@ from odoo import models, api
 class StudioMixin(models.AbstractModel):
     """ Mixin that overrides the create and write methods to properly generate
         ir.model.data entries flagged with Studio for the corresponding resources.
+        Doesn't create an ir.model.data if the record is part of a module being
+        currently installed as the ir.model.data will be created automatically
+        afterwards.
     """
     _name = 'studio.mixin'
 
@@ -14,7 +17,7 @@ class StudioMixin(models.AbstractModel):
     def create(self, vals):
         res = super(StudioMixin, self).create(vals)
 
-        if self._context.get('studio'):
+        if self._context.get('studio') and not self._context.get('install_mode'):
             res.create_studio_model_data(res.display_name)
 
         return res
@@ -23,7 +26,7 @@ class StudioMixin(models.AbstractModel):
     def write(self, vals):
         res = super(StudioMixin, self).write(vals)
 
-        if self._context.get('studio'):
+        if self._context.get('studio') and not self._context.get('install_mode'):
             for record in self:
                 record.create_studio_model_data(record.display_name)
 
