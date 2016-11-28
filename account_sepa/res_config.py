3e0c21de4f313c1290c22754dbce8d2c939bfbef
Yannick Tivisse <yti@odoo.com>
2016-11-28 09:07:40 +0100

    [FIX] account_sepa: Define a correct help message on `sepa_initiating_party_name`

--- account_sepa/res_config.py
+++ account_sepa/res_config.py
@@ -11,6 +11,6 @@ class AccountConfigSettings(models.TransientModel):
         help="Identification assigned by an institution (eg. VAT number).")
     sepa_orgid_issr = fields.Char(related='company_id.sepa_orgid_issr', string="Issuer *",
         help="Will appear in SEPA payments as the name of the party initiating the payment. Limited to 70 characters.")
-    sepa_initiating_party_name = fields.Char(related='company_id.sepa_initiating_party_name', string="Your Company Name *", help="Etherpad lite server. Example: beta.primarypad.com")
+    sepa_initiating_party_name = fields.Char(related='company_id.sepa_initiating_party_name', string="Your Company Name *", help="Name of the Creditor Reference Party. Usage Rule: Limited to 70 characters in length.")
     sepa_pain_version = fields.Selection(related='company_id.sepa_pain_version', string="SEPA Pain Version *",
                             help='SEPA may be a generic format, some countries differ from the SEPA recommandations made by the EPC (European Payment Councile) and thus the XML created need some tweakenings.')
