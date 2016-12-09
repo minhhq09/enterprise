d81dedbc518672cccdd58527ae9be590606fb66d
Pierre Masereel <pim@odoo.com>
2016-12-09 15:06:21 +0100

    [FIX] mail_push: fcm param in group_system
    Put the mail_push notification API keys, into the group
    'base.group_system', to hide the keys to unauthorized users.

--- mail_push/models/res_config.py
+++ mail_push/models/res_config.py
@@ -11,16 +11,16 @@ class FcmResConfig(models.TransientModel):
     @api.multi
     def set_fcm_api_key(self):
         fcm_api_key = self[0].fcm_api_key or ''
-        self.env['ir.config_parameter'].set_param('fcm_api_key', fcm_api_key)
+        self.env['ir.config_parameter'].set_param('fcm_api_key', fcm_api_key,  groups=['base.group_system'])
 
     @api.multi
     def set_fcm_project_id(self):
         fcm_project_id = self[0].fcm_project_id or ''
-        self.env['ir.config_parameter'].set_param('fcm_project_id', fcm_project_id)
+        self.env['ir.config_parameter'].set_param('fcm_project_id', fcm_project_id, groups=['base.group_system'])
 
     @api.multi
     def get_default_fcm_credentials(self, fields=None):
-        get_param = self.env['ir.config_parameter'].get_param
+        get_param = self.env['ir.config_parameter'].sudo().get_param
         fcm_api_key = get_param('fcm_api_key', default='')
         fcm_project_id = get_param('fcm_project_id', default='')
         return {
