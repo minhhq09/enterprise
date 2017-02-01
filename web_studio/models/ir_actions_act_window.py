73a9802e1a16e820784dc6f536f9cba3e07e0e8e
Martin Geubelle <mge@odoo.com>
2017-02-01 14:26:54 +0100

    [FIX] web_studio: keep changes on act_window.view
    All models editable with Studio inherit a mixin that set (or create)
    `noupdate` and `studio` on the corresponding External Identifier.
    The purpose is to keep the changes done with Studio after a module
    update.
    
    The poor model ´ir.actions.act_window.view´ had been forgotten but
    that's ancient history.

--- web_studio/models/ir_actions_act_window.py
+++ web_studio/models/ir_actions_act_window.py
@@ -7,3 +7,8 @@ from odoo import models
 class IrActionsActWindow(models.Model):
     _name = 'ir.actions.act_window'
     _inherit = ['studio.mixin', 'ir.actions.act_window']
+
+
+class IrActionsActWindowView(models.Model):
+    _name = 'ir.actions.act_window.view'
+    _inherit = ['studio.mixin', 'ir.actions.act_window.view']
