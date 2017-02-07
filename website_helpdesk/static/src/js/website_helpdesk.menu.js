2627fc5375f44866988b4496b9a91af90e922904
Nicolas Martinelli <nim@odoo.com>
2017-02-07 11:13:59 +0100

    [FIX] website_helpdesk: show more/show less
    In the user portal, the 'Show more' button used to show more tickets
    does not work.
    
    It's because nothing has been implemented to make it work.
    
    opw-705916

--- website_helpdesk/static/src/js/website_helpdesk.menu.js
+++ website_helpdesk/static/src/js/website_helpdesk.menu.js
@@ -9,4 +9,19 @@ odoo.define("website_helpdesk.menu", function (require) {
         $link = $link.filter("[href$='" + pathname + "']");
     }
     $link.first().closest("li").addClass("active");
+
+    // 'Show more' / 'Show less' buttons
+    $('.o_my_show_more').click(function(e) {
+        e.preventDefault();
+        $(e.target).addClass('hidden');
+        $('.o_my_show_less').removeClass('hidden');
+        $('.to_hide').removeClass('hidden');
+    });
+    $('.o_my_show_less').click(function(e) {
+        e.preventDefault();
+        $(e.target).addClass('hidden');
+        $('.o_my_show_more').removeClass('hidden');
+        $('.to_hide').addClass('hidden');
+    });
+
 });
