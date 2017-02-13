b56101200ac35dae3eadbc0bdf4ba17461f33907
Adrien Dieudonne <adr@odoo.com>
2017-02-13 11:29:21 +0100

    [FIX] web_studio: catch exception in studio view
    Issue:
    -----
    This commit prevents a traceback caused by a customized Studio view.
    The issue occured when you tried to edit a inherited view with groups on it.
    When an user didn't have access to it, the content of this view wasn't
    added in the arch. So even if the studio view is applied last,
    it wasn't possible to apply xpath on it.
    
    You may also have this problem if you uninstall a module.
    
    Example:
    -----
    You are connected as admin user and you open Studio.
    You are able to see elemens "A" and "B" because you are member
    of groups "a" and "b", respectively.
    You try to add a new element after the "A" element. So, an xpath will be
    generated from "A".
    At this step, everything works fine and you save the changes.
    
    Now, you are logged in with another user with the group "b" only.
    But the studio view doesn't find the "A" element and just crash.
    
    Fix:
    -----
    To fix this, we apply each xpath from the studio view with a fallback.
    In case of exception, the incident is only reported in the log, and
    the inheritance proceeds with the next xpath.
    
    We also warn users of possible problematic xpaths.
    These cases are the elements added with Studio and that are located after
    a node defined in a view protected by a group.
    
    Additional Information:
    -----
    There are several ways to restrict a part of the view to a group of users:
    
    (1) In the arch, you can use the "groups" attribute on a node to specify which
    user groups have the right to see the content of that node.
    The result will be an architecture rendered with html elements hidden
    by 'oe_invisible' class.
    
    (2) On the view, you can use the "groups_id" field to give view access
    to certain users only.
    The view will not be applied to the architecture if the user is not allowed
    to access it. So, nothing will be rendered.
    
    In the future:
    -----
    We will probably remove the "groups_id" field from the "ir.ui.view" template.
    We will find a way to use the "groups" attribute on some xpath node where it was
    not possible to do it.
    Example:
        <xpath expr="//node" position='attributes'>
            <attribute name='attr'>value</attribute>
        </xpath>
    
    Maybe we should split the studio view in several others:
    The studio view would then be a view inherited from the view you are
    trying to edit. Like we already did.
    But this doesn't solve the fact that we don't know in which group we have to add
    a new node that a user is trying to add.
    You can find this kind of situation when you try to add a new node after an
    element "A" introduced by a view protected by a group, but before an element "B"
    coming from another view without group restriction.

--- web_studio/models/ir_ui_view.py
+++ web_studio/models/ir_ui_view.py
@@ -47,3 +47,67 @@ class View(models.Model):
             'arch': arch,
             'name': "Default %s view for %s" % ('form', res_model),
         })
+
+    # Returns "true" if the view_id is the id of the studio view.
+    def _is_studio_view(self):
+        return self.xml_id.startswith('studio_customization')
+
+    # Based on inherit_branding of ir_ui_view
+    # This will add recursively the groups ids on the spec node.
+    def _groups_branding(self, specs_tree, view_id):
+        groups_id = self.browse(view_id).groups_id
+        if groups_id:
+            attr_value = ','.join(map(str, groups_id.ids))
+            for node in specs_tree.iter(tag=etree.Element):
+                if not node.get('position'):
+                    node.set('studio-view-group-ids', attr_value)
+
+    # Used for studio views only.
+    # This studio view specification will not always be available.
+    # So, we add the groups name to find out when they will be available.
+    # This information will be used in Studio to inform the user.
+    def _set_groups_info(self, node, group_ids):
+        groups = self.env['res.groups'].browse(map(int, group_ids.split(',')))
+        view_group_names = ','.join(groups.mapped('name'))
+        for child in node.iter(tag=etree.Element):
+            child.set('studio-view-group-names', view_group_names)
+            child.set('studio-view-group-ids', group_ids)
+
+    # Used for studio views only.
+    # Check if the hook node depends of groups.
+    def _check_parent_groups(self, source, spec):
+        node = self.locate_node(source, spec)
+        if node is not None and node.get('studio-view-group-ids'):
+            # Propogate group info for all children
+            self._set_groups_info(spec, node.get('studio-view-group-ids'))
+
+    # Used for studio views only.
+    # Apply spec by spec studio view.
+    def _apply_studio_specs(self, source, specs_tree, studio_view_id):
+        for spec in specs_tree.iterchildren(tag=etree.Element):
+            if self._context.get('studio'):
+                # Detect xpath base on a field added by a view with groups
+                self._check_parent_groups(source, spec)
+                # Here, we don't want to catch the exception.
+                # This mechanism doesn't save the view if something goes wrong.
+                source = super(View, self).apply_inheritance_specs(source, spec, studio_view_id)
+            else:
+                # Avoid traceback if studio view and skip xpath when studio mode is off
+                try:
+                    source = super(View, self).apply_inheritance_specs(source, spec, studio_view_id)
+                except ValueError:
+                    # 'locate_node' already log this error.
+                    pass
+        return source
+
+    def apply_inheritance_specs(self, source, specs_tree, inherit_id):
+        # Add branding for groups if studio mode is on
+        if self._context.get('studio'):
+            self._groups_branding(specs_tree, inherit_id)
+
+        # If this is studio view, we want to apply it spec by spec
+        if self.browse(inherit_id)._is_studio_view():
+            return self._apply_studio_specs(source, specs_tree, inherit_id)
+        else:
+            return super(View, self).apply_inheritance_specs(source, specs_tree, inherit_id)
+
