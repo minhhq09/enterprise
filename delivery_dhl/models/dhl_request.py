db36200ef2f7b8772cb8cd05d38f7c620698273c
mdietrichc2c <matthieu.dietrich@camptocamp.com>
2017-02-15 16:01:24 +0100

    [FIX] delivery_dhl: Unaccent PersonName in Consignee tag
    The label was correctly created by DHL,
    however the XML response returned an error:
    `ParseError: not well-formed (invalid token)`
    
    opw-708839
    Closes #783

--- delivery_dhl/models/dhl_request.py
+++ delivery_dhl/models/dhl_request.py
@@ -309,7 +309,7 @@ class DHLProvider():
         etree.SubElement(consignee_node, "CountryCode").text = param["recipient_partner"].country_id.code
         etree.SubElement(consignee_node, "CountryName").text = param["recipient_partner"].country_id.name
         contact_node = etree.SubElement(consignee_node, "Contact")
-        etree.SubElement(contact_node, "PersonName").text = param["recipient_partner"].name
+        etree.SubElement(contact_node, "PersonName").text = self._remove_accents(param["recipient_partner"].name)
         etree.SubElement(contact_node, "PhoneNumber").text = param["recipient_partner"].phone
         etree.SubElement(contact_node, "Email").text = param["recipient_partner"].email
         if param["is_dutiable"]:
