d3982a8291be17dee46da129290adcf26abdb0db
Richard Mathot <rim@odoo.com>
2017-03-13 09:03:24 +0100

    [FIX] l10_be_reports: fix the previous fix 8e19266ada12a4e14ee59932ac8cfb3490e41468
    The previous fix 8e19266ada12a4e14ee59932ac8cfb3490e41468 acted with
    good intentions BUT this query was built in a conditional way, that
    don't need to be escaped. That's why we build the query in 2 steps.

--- l10n_be_reports/models/partner_vat_listing.py
+++ l10n_be_reports/models/partner_vat_listing.py
@@ -22,28 +22,29 @@ class ReportL10nBePartnerVatListing(models.AbstractModel):
             company_clauses = ['AND l.company_id IN ' + company_ids, 'AND l2.company_id IN ' + company_ids]
         tag_ids = [self.env['ir.model.data'].xmlid_to_res_id(k) for k in ['l10n_be.tax_tag_00', 'l10n_be.tax_tag_01', 'l10n_be.tax_tag_02', 'l10n_be.tax_tag_03', 'l10n_be.tax_tag_45']]
         tag_ids_2 = [self.env['ir.model.data'].xmlid_to_res_id(k) for k in ['l10n_be.tax_tag_01', 'l10n_be.tax_tag_02', 'l10n_be.tax_tag_03']]
-        self.env.cr.execute("""SELECT sub1.partner_id, sub1.name, sub1.vat, sub1.turnover, sub2.vat_amount
+        query = """SELECT sub1.partner_id, sub1.name, sub1.vat, sub1.turnover, sub2.vat_amount
             FROM (SELECT l.partner_id, p.name, p.vat, SUM(l.credit - l.debit) as turnover
                   FROM account_move_line l
                   LEFT JOIN res_partner p ON l.partner_id = p.id
                   LEFT JOIN account_move_line_account_tax_rel amlt ON l.id = amlt.account_move_line_id
                   LEFT JOIN account_tax_account_tag tt on amlt.account_tax_id = tt.account_tax_id
-                  WHERE tt.account_account_tag_id IN %s
-                  AND l.partner_id IN %s
-                  AND l.date >= %s
-                  AND l.date <= %s
+                  WHERE tt.account_account_tag_id IN %%s
+                  AND l.partner_id IN %%s
+                  AND l.date >= %%s
+                  AND l.date <= %%s
                   %s
                   GROUP BY l.partner_id, p.name, p.vat) AS sub1
             LEFT JOIN (SELECT l2.partner_id, SUM(l2.credit - l2.debit) as vat_amount
                   FROM account_move_line l2
                   LEFT JOIN account_tax_account_tag tt2 on l2.tax_line_id = tt2.account_tax_id
-                  WHERE tt2.account_account_tag_id IN %s
-                  AND l2.partner_id IN %s
-                  AND l2.date >= %s
-                  AND l2.date <= %s
+                  WHERE tt2.account_account_tag_id IN %%s
+                  AND l2.partner_id IN %%s
+                  AND l2.date >= %%s
+                  AND l2.date <= %%s
                   %s
                   GROUP BY l2.partner_id) AS sub2 ON sub1.partner_id = sub2.partner_id
-                """, (tuple(tag_ids), tuple(partner_ids), context_id.date_from, context_id.date_to, company_clauses[0], tuple(tag_ids_2), tuple(partner_ids), context_id.date_from, context_id.date_to, company_clauses[1]))
+                """ % tuple(company_clauses)
+        self.env.cr.execute(query, (tuple(tag_ids), tuple(partner_ids), context_id.date_from, context_id.date_to, tuple(tag_ids_2), tuple(partner_ids), context_id.date_from, context_id.date_to))
         for record in self.env.cr.dictfetchall():
             currency_id = self.env.user.company_id.currency_id
             if not currency_id.is_zero(record['turnover']):
