minhhq09/enterprise

commit 45451b2c6e7e8070b9d77ed2ad92bce4d37bf5f9

[FIX] inter_company_rules: from po to so: getting pricelist should be done as sudo(intercompany_uid)

inter_company_rules/models/purchase_order.py
@@ -32,7 +32,6 @@ class purchase_order(models.Model):
         """
         self = self.with_context(force_company=company.id)
         SaleOrder = self.env['sale.order']
-        company_partner = self.company_id.partner_id
 
         # find user for creating and validation SO/PO from partner company
         intercompany_uid = company.intercompany_user_id and company.intercompany_user_id.id or False
@@ -43,6 +42,7 @@ class purchase_order(models.Model):
             raise Warning(_("Inter company user of company %s doesn't have enough access rights") % company.name)
 
         # check pricelist currency should be same with SO/PO document
+        company_partner = self.sudo(intercompany_uid).company_id.partner_id
         if self.currency_id.id != company_partner.property_product_pricelist.currency_id.id:
             raise Warning(_('You cannot create SO from PO because sale price list currency is different than purchase price list currency.'))
 

commit 55db26c571c3da7debb6bec099b0581b508c71e3

[FIX] inter_company_rules: get the other company's view point
commit 45451b2c6e7e8070b9d77ed2ad92bce4d37bf5f9 introduced a bug while solving another one.
First bug opw 727477: When in A we do an PO on B, a SO will be created in B
the SO must take A as its customer, as viewed from B (to get the pricelist right)
BUT we are currently in A.
HENCE we need to partner_A.sudo(B_intercompany_user_uid) to make it right.

Second bug that the fix has brought:
If the intercompany_users are not part of the other company as well,
we end up trying to access A's PO with userB's rights, which raises an error.

In the context of sharing partners between companies, we can safely go
in A from the company of the PO to the partner representing A, but viewed from B
in order to make a SO

This also works when partners are not shared BUT both A's and B's partners has company _id = Null

OPW 743900
and
OPW 727477

Closes #1035

inter_company_rules/models/purchase_order.py
@@ -42,7 +42,7 @@ class purchase_order(models.Model):
             raise Warning(_("Inter company user of company %s doesn't have enough access rights") % company.name)
 
         # check pricelist currency should be same with SO/PO document
-        company_partner = self.sudo(intercompany_uid).company_id.partner_id
+        company_partner = self.company_id.partner_id.sudo(intercompany_uid)
         if self.currency_id.id != company_partner.property_product_pricelist.currency_id.id:
             raise Warning(_('You cannot create SO from PO because sale price list currency is different than purchase price list currency.'))
 