59a56c523d33e883c4e0d494645992d02f8f9f1e
Lucas Perais (lpe) <lpe@odoo.com>
2017-05-24 12:00:18 +0200

    [FIX] account_bank_statement_import_camt: allow importing non-unique 0+ transaction number
    Before this commit, it was impossible to import camt files with multiple foreign statement
    which AcctSvcrRef may be full of zeros; breaking the unique import id
    
    After this commit we account for this and provide a unique import id anyway
    
    OPW 744391
    
    Closes #1045

--- account_bank_statement_import_camt/wizard/account_bank_statement_import_camt.py
+++ account_bank_statement_import_camt/wizard/account_bank_statement_import_camt.py
@@ -3,6 +3,7 @@
 
 from lxml import etree
 from StringIO import StringIO
+import re
 
 from odoo import models
 
@@ -26,6 +27,11 @@ class AccountBankStatementImport(models.TransientModel):
         return super(AccountBankStatementImport, self)._parse_file(data_file)
 
     def _parse_file_camt(self, root):
+
+        def is_full_of_zeros(strg):
+            pattern_zero = re.compile('^0+$')
+            return bool(pattern_zero.match(strg))
+
         ns = {k or 'ns': v for k, v in root.nsmap.iteritems()}
         statement_list = []
         for statement in root[0].findall('ns:Stmt', ns):
@@ -78,7 +84,9 @@ class AccountBankStatementImport(models.TransientModel):
                                       """, namespaces=ns)
                 entry_vals['ref'] = ref and ref[0] or False
                 unique_import_ref = entry.xpath('ns:AcctSvcrRef/text()', namespaces=ns)
-                entry_vals['unique_import_id'] = unique_import_ref and unique_import_ref[0] or statement_vals['name'] + '-' + str(sequence)
+                entry_vals['unique_import_id'] = (unique_import_ref and
+                                                  (unique_import_ref[0] if not is_full_of_zeros(unique_import_ref[0]) else False) or
+                                                  statement_vals['name'] + '-' + str(sequence))
 
                 transactions.append(entry_vals)
             statement_vals['transactions'] = transactions
