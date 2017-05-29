d51eb05fd8f8909265a85ca6a2aac4622f43fcba
Raphael Collet <rco@odoo.com>
2017-05-29 16:08:57 +0200

    [FIX] account_reports: avoid dangling menu entries after module uninstallation
    The model `account.financial.html.report` creates dynamic action and menu
    entries.  In order to automatically remove those menu entries when uninstalling
    `account_reports`, add an XML id to those action and menu entries.
    
    opw-740175
    opw-710381

--- account_reports/models/account_financial_report.py
+++ account_reports/models/account_financial_report.py
@@ -31,7 +31,10 @@ class ReportAccountFinancialReport(models.Model):
     tax_report = fields.Boolean('Tax Report', help="Set to True to automatically filter out journal items that have the boolean field 'tax_exigible' set to False")
 
     def create_action_and_menu(self, parent_id):
-        client_action = self.env['ir.actions.client'].create({
+        # create action and menu with corresponding external ids, in order to
+        # remove remove those entries when deinstalling module 'account_reports'
+        IMD = self.env['ir.model.data']
+        action_vals = {
             'name': self.get_title(),
             'tag': 'account_report_generic',
             'context': {
@@ -39,12 +42,16 @@ class ReportAccountFinancialReport(models.Model):
                 'model': 'account.financial.html.report',
                 'id': self.id,
             },
-        })
-        self.env['ir.ui.menu'].create({
+        }
+        action_id = IMD._update('ir.actions.client', 'account_reports', action_vals,
+                                'account_financial_html_report_action_' + str(self.id))
+        menu_vals = {
             'name': self.get_title(),
-            'parent_id': parent_id or self.env['ir.model.data'].xmlid_to_res_id('account.menu_finance_reports'),
-            'action': 'ir.actions.client,%s' % (client_action.id,),
-        })
+            'parent_id': parent_id or IMD.xmlid_to_res_id('account.menu_finance_reports'),
+            'action': 'ir.actions.client,%s' % (action_id,),
+        }
+        IMD._update('ir.ui.menu', 'account_reports', menu_vals,
+                    'account_financial_html_report_menu_' + str(self.id))
         self.write({'menuitem_created': True})
 
     @api.model
