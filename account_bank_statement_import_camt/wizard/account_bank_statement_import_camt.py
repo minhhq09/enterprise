ff84f8b78857b8f86dde228116f5ceecb24b7d60
Nicolas Lempereur <nle@odoo.com>
2017-06-22 17:57:26 +0200

    [FIX] account_bank_statement_import_camt: also use additional info
    If no "Description" data is available, try to set the description
    instead to the optional "Transaction Information" and if not available
    either, then keep previous behavior and have just "/".
    
    opw-747686
    closes #1134

--- account_bank_statement_import_camt/wizard/account_bank_statement_import_camt.py
+++ account_bank_statement_import_camt/wizard/account_bank_statement_import_camt.py
@@ -76,6 +76,7 @@ class AccountBankStatementImport(models.TransientModel):
 
                 # Name 0..1
                 transaction_name = entry.xpath('.//ns:RmtInf/ns:Ustrd/text()', namespaces=ns)
+                transaction_name = transaction_name or entry.xpath('ns:AddtlNtryInf/text()', namespaces=ns)
                 partner_name = entry.xpath('.//ns:RltdPties/ns:%s/ns:Nm/text()' % (counter_party,), namespaces=ns)
                 entry_vals['name'] = transaction_name and transaction_name[0] or '/'
                 entry_vals['partner_name'] = partner_name and partner_name[0] or False
